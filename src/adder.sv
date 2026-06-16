2f475bf0e2bde4afcb638a1f61199f4dfeff53be
mwasko5 <mwasko@arizona.edu>
2026-06-16 11:10:45 -0700

    adder

---
+++ src/adder.sv
@@ -0,0 +1,11 @@
+module adder #(
+    parameter int DATA_WIDTH = 64 // default width of 64
+) (
+    input logic [DATA_WIDTH - 1:0] INPUT_A, INPUT_B,
+
+    output logic [DATA_WIDTH - 1:0] OUTPUT
+);
+
+    assign OUTPUT = INPUT_A + INPUT_B;
+
+endmodule
